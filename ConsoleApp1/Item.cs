822dd5d44bfe3f44de3496810338fee40974eaf2
HYUNWOO-Unity <janghyunwoo6654@naver.com>
2024-09-27 10:13:41 +0900

    Update.Item.Cs
    Item 능력치 설명 변경

--- ConsoleApp1/Item.cs
+++ ConsoleApp1/Item.cs
@@ -57,13 +57,13 @@
                 new Item("체력 회복 물약", "사용 시 체력을 50 회복", ItemType.Potion, 0, 0, 50, 0, 0, 0, 0),
                 new Item("마나 회복 물약", "사용 시 마나를 50 회복", ItemType.Potion, 0, 0, 0, 50, 0, 0, 0),
                 new Item("엘릭서", "사용 시 체력 100 마나를 50 회복", ItemType.Potion, 0, 0, 100, 50, 0, 0, 0),
-                new Item("검", "기본 공격력 10 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.2, 20),
-                new Item("단검", "기본 공격력 14 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.5, 35),
-                new Item("활", "기본 공격력 12 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.3, 30),
-                new Item("지팡이", "기본 공격력 13 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.3, 25),
+                new Item("검", "기본 공격력 25, 크리티컬 확률 2, 크리티컬 데미지 20 증가", ItemType.Weapon, 25, 0, 0, 0, 0, 0.2, 20),
+                new Item("단검", "기본 공격력 35,크리티컬 확률 5, 크리티컬 데미지 35 증가", ItemType.Weapon, 35, 0, 0, 0, 0, 0.5, 35),
+                new Item("활", "기본 공격력 30 크리티컬 확률 3, 크리티컬 데미지 30 증가", ItemType.Weapon, 30, 0, 0, 0, 0, 0.3, 30),
+                new Item("지팡이", "기본 공격력 25 크리티컬 확률3, 크리티컬 데미지 25 증가", ItemType.Weapon, 25, 0, 0, 0, 0, 0.3, 25),
                 new Item("강철 투구", "방어력 5 증가", ItemType.Head, 0, 5, 0, 0, 0, 0, 0),
                 new Item("가죽 갑옷", "방어력 10 증가", ItemType.Top, 0, 10, 0, 0, 0, 0, 0),
-                new Item("강철 바지", "방어력 7 증가", ItemType.Bottom, 0, 7, 0, 0, 0, 0, 0)
+                new Item("강철 바지", "방어력 7 증가", ItemType.Bottom, 0, 7, 0, 0, 0, 0, 0)          
             };
         }
     }
