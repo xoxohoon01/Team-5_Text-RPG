684418ee0b12118fd3566c723ff4c6e4c2b8f59d
HYUNWOO-Unity <janghyunwoo6654@naver.com>
2024-09-30 16:56:29 +0900

    Update Item.cs
    변경

--- Text_RPG/Item.cs
+++ Text_RPG/Item.cs
@@ -49,24 +49,7 @@
             {
                 new Item("체력 회복 물약", "사용 시 체력을 50 회복", ItemType.Potion, 0, 0, 50, 0, 0, 0, 0),
                 new Item("마나 회복 물약", "사용 시 마나를 50 회복", ItemType.Potion, 0, 0, 0, 50, 0, 0, 0),
-                new Item("엘릭서", "사용 시 체력 100 마나를 50 회복", ItemType.Potion, 0, 0, 100, 50, 0, 0, 0),
-<<<<<<<< HEAD:ConsoleApp1/Item.cs
-                new Item("검", "기본 공격력 20, 크리티컬 확률 2, 크리티컬 데미지 15 증가", ItemType.Weapon, 20, 0, 0, 0, 0, 0.2, 15),
-                new Item("강철 검", "기본 공격력 25, 크리티컬 확률 3, 크리티컬 데미지 20 증가", ItemType.Weapon, 25, 0, 0, 0, 0, 0.3, 20),
-                new Item("단검", "기본 공격력 35,크리티컬 확률 4, 크리티컬 데미지 30 증가", ItemType.Weapon, 30, 0, 0, 0, 0, 0.4, 30),
-                new Item("강철 단검", "기본 공격력 35,크리티컬 확률 5, 크리티컬 데미지 35 증가", ItemType.Weapon, 35, 0, 0, 0, 0, 0.5, 35),
-                new Item("활", "기본 공격력 25,크리티컬 확률 3, 크리티컬 데미지 20 증가", ItemType.Weapon, 25, 0, 0, 0, 0, 0.3, 20),
-                new Item("단궁", "기본 공격력 20 크리티컬 확률 3, 크리티컬 데미지 30 증가", ItemType.Weapon, 20, 0, 0, 0, 10, 0.3, 30),
-                new Item("장궁", "기본 공격력 30 크리티컬 확률 5, 크리티컬 데미지 35 증가", ItemType.Weapon, 30, 0, 0, 0, 0, 0.5, 35),
-                new Item("지팡이", "기본 공격력 25 크리티컬 확률3, 크리티컬 데미지 20 증가", ItemType.Weapon, 25, 0, 0, 0, 0, 0.3, 20),
-                new Item("고대 지팡이", "기본 공격력 35 크리티컬 확률5, 크리티컬 데미지 35 증가", ItemType.Weapon, 35, 0, 0, 0, 0, 0.5, 35),
-                new Item("강철 투구", "방어력 10 증가", ItemType.Head, 0, 10, 0, 0, 0, 0, 0),
-                new Item("미스릴 투구", "방어력 15 증가", ItemType.Head, 0, 15, 0, 0, 0, 0, 0),
-                new Item("가죽 갑옷", "방어력 5, 스피드 5 증가", ItemType.Top, 0, 5, 0, 0, 5, 0, 0),
-                new Item("미스릴 갑옷", "방어력 20 증가", ItemType.Top, 0, 20, 0, 0, 5, 0, 0),
-                new Item("강철 바지", "방어력 7 증가", ItemType.Bottom, 0, 7, 0, 0, 0, 0, 0),          
-                new Item("미스릴 바지", "방어력 17 증가", ItemType.Bottom, 0, 17, 0, 0, 0, 0, 0)          
-========
+                new Item("엘릭서", "사용 시 체력 100 마나를 50 회복", ItemType.Potion, 0, 0, 100, 50, 0, 0, 0),                     
                 new Item("검", "기본 공격력 10 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.2, 20),
                 new Item("단검", "기본 공격력 14 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.5, 35),
                 new Item("활", "기본 공격력 12 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.3, 30),
@@ -74,7 +57,6 @@
                 new Item("강철 투구", "방어력 5 증가", ItemType.Head, 0, 5, 0, 0, 0, 0, 0),
                 new Item("가죽 갑옷", "방어력 10 증가", ItemType.Top, 0, 10, 0, 0, 0, 0, 0),
                 new Item("강철 바지", "방어력 7 증가", ItemType.Bottom, 0, 7, 0, 0, 0, 0, 0)
->>>>>>>> dev:Text_RPG/Item.cs
             };
         }
     }
@@ -86,11 +68,7 @@
         Weapon,  // 무기
         Head,    // 머리 장비
         Top,   // 상의 장비
-<<<<<<<< HEAD:ConsoleApp1/Item.cs
         Bottom    // 하의 장비
-========
-        Bottom  // 하의 장비
->>>>>>>> dev:Text_RPG/Item.cs
     }
 }
    
