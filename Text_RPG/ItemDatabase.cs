9f320c80e7a3b4d3bd4148b470635653c709a394
BL4CK DoG <xoxohoon01@gmail.com>
2024-09-30 11:51:52 +0900

    아이템 데이터베이스 사전작업

---
+++ Text_RPG/ItemDatabase.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace TextRPG
+{
+    /*
+
+     Program.cs에서 
+     ItemDatabase 인스턴스를 만들고 
+     직렬화 및 역직렬화로 저장 및 부러오기 구현할 것
+
+     ItemDatabase itemDatabase = new ItemDatabase();
+     public void LoadItemDatabase()
+     {
+         if (File.Exists("./ItemDatabase.json"))
+         {
+             itemDatabase = JsonConvert.DeserializeObject<ItemDatabase>(File.ReadAllText("./ItemDatabase.json"));
+         }
+         else
+         {
+             itemDatabase.ITEM.Add(new Item());
+             SaveItemDatabase();
+         }
+     }
+     public void SaveItemDatabase()
+     {
+         string content = JsonConvert.SerializeObject(itemDatabase, Formatting.Indented);
+         File.WriteAllText("./ItemDatabase.json", content);
+     }
+
+     */
+    public class ItemDatabase
+    {
+        public List<Item> ITEM = new List<Item>();
+    }
+}
+   
