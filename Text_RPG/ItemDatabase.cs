b3edf8610473e5cb3558feba9a1148d1802a2f8a
BL4CK DoG <xoxohoon01@gmail.com>
2024-09-30 19:03:34 +0900

    아이템데이터베이스 수정

--- Text_RPG/ItemDatabase.cs
+++ Text_RPG/ItemDatabase.cs
@@ -14,14 +14,14 @@ namespace TextRPG
             InitItemDatabase();
         }
 
-        //데이터베이스 (Program.cs에서 메소드 호출할 것)
+        //데이터베이스 (Program.cs에서 메소드 호출할 것) (n줄 - 20이 아이템 번호)
         public void InitItemDatabase()
         {
             ITEM.Add(new Item());
             ITEM.Add(new Item("체력 회복 물약", "사용 시 체력을 50 회복", ItemType.Potion, 0, 0, 50, 0, 0, 0, 0));
             ITEM.Add(new Item("마나 회복 물약", "사용 시 마나를 50 회복", ItemType.Potion, 0, 0, 0, 50, 0, 0, 0));
             ITEM.Add(new Item("엘릭서", "사용 시 체력 100 마나를 50 회복", ItemType.Potion, 0, 0, 100, 50, 0, 0, 0));
-            ITEM.Add(new Item("검", "기본 공격력 10 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.2, 20));
+            ITEM.Add(new Item("검", "평범한 검이다.", ItemType.Weapon, 10, 0, 0, 0, 0, 0.2, 20));
             ITEM.Add(new Item("단검", "기본 공격력 14 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.5, 35));
             ITEM.Add(new Item("활", "기본 공격력 12 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.3, 30));
             ITEM.Add(new Item("지팡이", "기본 공격력 13 증가", ItemType.Weapon, 10, 0, 0, 0, 0, 0.3, 25));
