911c5462d68c249ec3dc43dedc70449db6cb6ae5
HYUNWOO-Unity <janghyunwoo6654@naver.com>
2024-09-30 10:34:21 +0900

    Update Enemy.cs
    고블린, 오크 추가 밑 배틀 시스템

--- Text_RPG/Enemy.cs
+++ Text_RPG/Enemy.cs
@@ -2,22 +2,102 @@
 {
     class Enemy : Unit
     {
-        public Enemy(string _name = "", int _hp = 0, int _mp = 0, int _damage = 0, int _armor = 0, int _speed = 0, int _critChance = 0, int _critDamage = 0)
-        {
-            name = _name;
+        public string Name;
+        public int Hp, MaxHp;
+        public int Mp, MaxMp;
+        public int Damage, Armor, Speed;
+        public int CritChance, CritDamage;
 
-            hp = _hp;
-            maxHp = _hp;
-            mp = _mp;
-            maxMp = _mp;
+        public bool IsAlive => hp > 0;
+        public Enemy(string type) { 
+            switch (type.ToLower())
+            {
+                case "goblin":
+                    name = "Goblin";
+                    hp = 50;
+                    maxHp = 50;
+                    mp = 10;
+                    maxMp = 10;
+                    damage = 15;
+                    armor = 5;
+                    speed = 8;
+                    critChance = 10;
+                    critDamage = 20;
+                    break;
 
-            damage = _damage;
-            armor = _armor;
-            speed = _speed;
-            critChance = _critChance;
-            critDamage = _critDamage;
+                case "orc":
+                    name = "Orc";
+                    hp = 100;
+                    maxHp = 100;
+                    mp = 5;
+                    maxMp = 5;
+                    damage = 25;
+                    armor = 15;
+                    speed = 4;
+                    critChance = 5;
+                    critDamage = 30;
+                    break;
+
+                default:
+                    name = "Unknown";
+                    hp = 0;
+                    maxHp = 0;
+                    mp = 0;
+                    maxMp = 0;
+                    damage = 0;
+                    armor = 0;
+                    speed = 0;
+                    critChance = 0;
+                    critDamage = 0;
+                    break;
+            }
         }
-    
+        void Battle(Player player, Unit enemy)
+        {
+            // 속도가 높은 유닛이 먼저 공격
+            bool playerTurn = player.Speed >= enemy.speed;
+
+            while (player.Hp > 0 && enemy.hp > 0)
+            {
+                if (playerTurn)
+                {
+                    // 플레이어가 먼저 공격
+                    int actualDamage = player.Damage - enemy.armor;
+                    if (actualDamage < 0) actualDamage = 0;
+
+                    enemy.hp -= actualDamage;
+                    Console.WriteLine($"{player.Name} attacks {enemy.name} for {actualDamage} damage!");
+
+                    if (enemy.hp <= 0)
+                    {
+                        Console.WriteLine($"{enemy.name} has been defeated!");
+                        break;
+                    }
+                }
+                else
+                {
+                    // 적이 먼저 공격
+                    int actualDamage = enemy.damage - player.Defense;
+                    if (actualDamage < 0) actualDamage = 0;
+
+                    player.Hp -= actualDamage;
+                    Console.WriteLine($"{enemy.name} attacks {player.Name} for {actualDamage} damage!");
+
+                    if (player.Hp <= 0)
+                    {
+                        Console.WriteLine($"{player.Name} has been defeated!");
+                        break;
+                    }
+                }
+
+                // 턴 교체
+                playerTurn = !playerTurn;
+
+                // 상태 출력 (선택 사항)
+                Console.WriteLine($"{player.Name} HP: {player.Hp} / {player.MaxHp}");
+                Console.WriteLine($"{enemy.name} HP: {enemy.hp} / {enemy.maxHp}");
+            }
+      }
         public void OnAttack(ref Player _player)
         {
             _player.Hp = damage - (_player.Defense + _player.TotalDefenseBonus());
