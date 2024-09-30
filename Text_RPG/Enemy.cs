23b658e1104f354cfa941f78bb4997e37a7aac0e
Lim-seok <lsg15951@gmail.com>
2024-09-30 09:55:14 +0900

    Player.cs
    0

--- Text_RPG/Enemy.cs
+++ Text_RPG/Enemy.cs
@@ -2,20 +2,25 @@
 {
     class Enemy : Unit
     {
-        public Enemy(string _name = "")
+        public Enemy(string _name = "", int _hp = 0, int _mp = 0, int _damage = 0, int _armor = 0, int _speed = 0, int _critChance = 0, int _critDamage = 0)
         {
             name = _name;
 
-            hp = 0;
-            maxHp = 0;
-            mp = 0;
-            maxMp = 0;
+            hp = _hp;
+            maxHp = _hp;
+            mp = _mp;
+            maxMp = _mp;
 
-            damage = 0;
-            armor = 0;
-            speed = 0;
-            critChance = 0;
-            critDamage = 0;
+            damage = _damage;
+            armor = _armor;
+            speed = _speed;
+            critChance = _critChance;
+            critDamage = _critDamage;
+        }
+    
+        public void OnAttack(ref Player _player)
+        {
+            _player.Hp = damage - (_player.Defense + _player.TotalDefenseBonus());
         }
     }
 }
